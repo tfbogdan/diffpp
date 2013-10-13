bc24812343a488956f64bd59e02ccb3f3d7d8391
bogdan tudoran <tfbogdan@windowslive.com>
2013-10-13 22:04:51 +0300

    added quicktest file - has to be compiled with -std=c++11

--- diffpp/script.hpp
+++ diffpp/script.hpp
@@ -62,71 +62,61 @@ namespace diffpp {
 			typedef std::map< int, int > kdmap_t;
 			typedef std::vector< kdmap_t > kdmap_container_t;
 
-			struct longestpath_fordistance_fwd { 
+			struct search_forward { 
 				template < typename fwdItA, typename fwdItB, typename predicate > inline
-				static bool exec ( const fwdItA A0, const fwdItB B0, const int sizeA, const int sizeB, kdmap_t &kdmap, const int distance, predicate eq ) {
-					const int inv_dist = distance * -1; 
-					for ( int kline = inv_dist; kline <= distance; kline += 2 ) {
-						bool down = ( inv_dist == kline || ( kline != distance && kdmap[kline-1] < kdmap[kline+1] ) );
-						
-						int prev_kline = kline + ( down ? 1 : -1 );
+				static bool exec ( const fwdItA A0, const fwdItB B0, const int sizeA, const int sizeB, kdmap_t &kdmap, const int distance, predicate eq, const int delta_ab, const int inv_dist, const int kline ) {
+					bool down = ( inv_dist == kline || ( kline != distance && kdmap[kline-1] < kdmap[kline+1] ) );
+					int prev_kline = kline + ( down ? 1 : -1 );
 
-						point start; start.x = kdmap[prev_kline];
-						start.y = start.x - prev_kline;
+					point start; start.x = kdmap[prev_kline];
+					start.y = start.x - prev_kline;
 
-						point end; end.x = start.x + ( down ? 0 : 1 );	
-						end.y = end.x - kline;
+					point end; end.x = start.x + ( down ? 0 : 1 );	
+					end.y = end.x - kline;
 
-						fwdItA itA( A0 ); fwdItB itB( B0 );
-						std::advance( itA, end.x );
-						std::advance( itB, end.y );
+					fwdItA itA( A0 ); fwdItB itB( B0 );
+					std::advance( itA, end.x );
+					std::advance( itB, end.y );
 
-						while ( end.x < sizeA && end.y < sizeB && eq(*itA, *itB) ) { 
-							++end.x;
-							++end.y;
-							++itA; ++itB;
-						}
-
-						kdmap[kline] = end.x;
-
-						if ( end.x >= sizeA && end.y >= sizeB ) return true; // solution found
+					while ( end.x < sizeA && end.y < sizeB && eq(*itA, *itB) ) { 
+						++end.x;
+						++end.y;
+						++itA; ++itB;
 					}
+
+					kdmap[kline] = end.x;
+
+					if ( end.x >= sizeA && end.y >= sizeB ) return true; // solution found
 					return false;
 				}
 			};
 
-			struct longestpath_fordistance_bwd { 
+			struct search_backward { 
 				template < typename fwdItA, typename fwdItB, typename predicate  > inline
-				static bool exec ( const fwdItA A0, const fwdItB B0, const int sizeA, const int sizeB, kdmap_t &kdmap, int distance, predicate eq ) {
-					const int delta = sizeA - sizeB;
-					const int inv_dist = distance * -1;
+				static bool exec ( const fwdItA A0, const fwdItB B0, const int sizeA, const int sizeB, kdmap_t &kdmap, int distance, predicate eq, const int delta_ab, const int inv_dist, const int kline ) {
+					bool up = ( kline == distance + delta_ab || ( kline != (inv_dist + delta_ab) && kdmap[kline-1] < kdmap[kline+1] ) );
+					const int prev_kline = kline + ( up ? -1 : 1 );
 
-					for ( int kline = inv_dist + delta; kline <= distance + delta; kline += 2 ) {
-						bool up = ( kline == distance + delta || ( kline != (inv_dist + delta) && kdmap[kline-1] < kdmap[kline+1] ) );
-						
-						const int prev_kline = kline + ( up ? -1 : 1 );
+					point start;start.x = kdmap[ prev_kline ];
+					start.y = start.x - prev_kline;
 
-						point start;start.x = kdmap[ prev_kline ];
-						start.y = start.x - prev_kline;
+					point end; end.x = up ? start.x : start.x - 1;
+					end.y = end.x - kline;
 
-						point end; end.x = up ? start.x : start.x - 1;
-						end.y = end.x - kline;
-
-						fwdItA itA(A0);
-						fwdItB itB(B0);
-						std::advance(itA, end.x-1);
-						std::advance(itB, end.y-1);
-						
-						while ( end.x > 0 && end.y > 0 && eq(*itA, *itB) ) {
-							--end.x;
-							--end.y;
-							--itA;--itB;
-						}
-
-						kdmap[kline] = end.x;
-
-						if ( end.x <= 0 && end.y <= 0 ) return true;
+					fwdItA itA(A0);
+					fwdItB itB(B0);
+					std::advance(itA, end.x-1);
+					std::advance(itB, end.y-1);
+					
+					while ( end.x > 0 && end.y > 0 && eq(*itA, *itB) ) {
+						--end.x;
+						--end.y;
+						--itA;--itB;
 					}
+
+					kdmap[kline] = end.x;
+
+					if ( end.x <= 0 && end.y <= 0 ) return true;
 					return false;
 				}
 			};
@@ -161,7 +151,7 @@ namespace diffpp {
 				template < typename container_t > 
 				static void solve ( kdmap_container_t &kdmaps, container_t &sln, const int sizeA, const int sizeB ) {
 					point current(0,0);
-					const int delta = sizeA-sizeB;
+					const int delta_ab = sizeA-sizeB;
 					
           for ( int distance = kdmaps.size() -1; current.x < sizeA || current.y < sizeB; --distance ) {
 						kdmap_t &kdmap( kdmaps[distance] );
@@ -171,7 +161,7 @@ namespace diffpp {
 						point end; end.x = kdmap[kline];
 						end.y = end.x - kline;
 
-						bool up = ( kline == distance + delta || ( kline != (inv_dist + delta) && kdmap[kline-1] < kdmap[kline+1] ) );
+						bool up = ( kline == distance + delta_ab || ( kline != (inv_dist + delta_ab) && kdmap[kline-1] < kdmap[kline+1] ) );
 						const int prev_kline = kline - ( up ? 1 : -1 );
 
 						point start; start.x = kdmap[prev_kline];
@@ -185,20 +175,25 @@ namespace diffpp {
 				}
 			};
 
-			template < typename calc_longestpath, typename calc_solutionfct, typename fwdItA, typename fwdItB, typename container_t, typename predicate > inline
-			void diff_greedy ( fwdItA A0, fwdItB B0, container_t &solution_out, kdmap_t &kdmap, const int sizeA, const int sizeB, predicate eq ) { 
+			template < typename search_dir, typename solve_dir, typename fwdItA, typename fwdItB, typename container_t, typename predicate > inline
+			void diff_greedy ( fwdItA A0, fwdItB B0, container_t &solution_out, kdmap_t &kdmap, const int sizeA, const int sizeB, predicate eq, bool dir ) { 
 				kdmap_container_t path_maps;
 				bool solution_found = false;
 				const int worst_case = sizeA + sizeB;
+				const int delta_ab = dir ? 0 : sizeA - sizeB;
 
 				for ( int distance(0); distance <= worst_case; ++distance ) {
-					solution_found = calc_longestpath::exec(A0, B0, sizeA, sizeB, kdmap, distance, eq );
+					const int inv_dist = distance * (-1);
+					for ( int kline = inv_dist + delta_ab; kline <= distance + delta_ab; kline += 2 ) {
+						solution_found = search_dir::exec(A0, B0, sizeA, sizeB, kdmap, distance, eq, delta_ab, inv_dist, kline );
+						if ( solution_found ) break;
+					}
 					path_maps.push_back(kdmap);
 					if ( solution_found ) break;
 				}
 
 				if ( solution_found )
-					calc_solutionfct::solve( path_maps, solution_out, sizeA, sizeB );
+					solve_dir::solve( path_maps, solution_out, sizeA, sizeB );
 				
 			}
 		}
@@ -209,8 +204,8 @@ namespace diffpp {
 			static const int sizeB = std::distance( B0, Bm );
 			detail::kdmap_t kdmap;
 			kdmap[1] = 0;
-			detail::diff_greedy<detail::longestpath_fordistance_fwd,
-								detail::solver_greedyfwd> ( A0, B0, sln, kdmap, sizeA, sizeB, eq );		
+			detail::diff_greedy<detail::search_forward,
+								detail::solver_greedyfwd> ( A0, B0, sln, kdmap, sizeA, sizeB, eq, true );		
 		}
 
 		template < typename fwdItA, typename fwdItB, typename container_t, typename predicate=std::equal_to<const typename fwdItA::reference > > inline
@@ -219,8 +214,8 @@ namespace diffpp {
 			static const int sizeB = std::distance( B0, Bm );
 			detail::kdmap_t kdmap;
 			kdmap[ sizeA - sizeB - 1 ] = sizeA;
-			detail::diff_greedy<detail::longestpath_fordistance_bwd,
-								detail::solver_greedybwd> ( A0, B0, sln, kdmap, sizeA, sizeB, eq );		
+			detail::diff_greedy<detail::search_backward,
+								detail::solver_greedybwd> ( A0, B0, sln, kdmap, sizeA, sizeB, eq, false );		
 		}
 
 	}	//::diff::algorithms
---
+++ tst/diffpp_test/main.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+
+
+
+#include <string>
+
+#include <vector>
+
+#include <diffpp/diffpp.hpp>
+
+std::string leftstr="ABCABBA";
+std::string rightstr="CBABAC";
+
+template < typename container_t > inline void print_diff_merge( const container_t &sln ) {
+    std::string merged;
+    for ( auto iter(sln.begin()); iter != sln.end(); ++iter ) {
+        diffpp::point start = iter->get_start();
+        diffpp::edit::command_t comm = iter->get_command();
+        int dir = iter->get_direction();
+        int matches = iter->get_matches();
+        diffpp::point end = iter->get_end();
+
+        int idxA = ~dir ? start.x : end.x;
+        int idxB = ~dir ? start.y : end.y;
+
+        if ( ~dir ) {
+            if ( comm == diffpp::edit::DELETE && idxA >= 0 ) {
+                    std::cout << leftstr[idxA] << " -> "<< leftstr[idxA]<< std::endl;
+                    merged.push_back(leftstr[idxA]);
+                    ++idxA;
+            } else {
+                if ( idxB >= 0 ) {
+                    std::cout << "     " << rightstr[idxB] << " <- " << rightstr[idxB] << std::endl;
+                    merged.push_back(rightstr[idxB]);
+                }
+                ++idxB;
+            }
+        }
+
+        int step(0);
+        for ( ; step < matches; ++step ) {
+            char a = ' ';
+            if ( idxA+step >= 0 && idxA+step < leftstr.size() ) {
+                std::cout << leftstr[idxA+step]<< " -> ";
+                a = leftstr[idxA+step];
+            } else {
+                std::cout << "     ";
+                a = rightstr[idxB+step];
+            }
+
+            merged.push_back(a);
+            std::cout << a;
+
+            if ( idxB+step >= 0 && idxB+step < rightstr.size() ) {
+                std::cout << " <- " << rightstr[idxB+step]<<std::endl;
+            } else {
+                std::cout << std::endl;
+            }
+        }
+
+        if ( !(~dir) ) {
+            if ( comm == diffpp::edit::DELETE && idxA+step < static_cast<int>(leftstr.size()) ) {
+                std::cout<< leftstr[idxA+step] << " -> " << leftstr[idxA+step]<< std::endl;
+                merged.push_back(leftstr[idxA+step]);
+            } else if( idxB+step < static_cast<int>(rightstr.size()) ) {
+                std::cout<< "     " << rightstr[idxB+step] << " <- " << rightstr[idxB+step] << std::endl;
+                merged.push_back(rightstr[idxB+step]);
+            }
+        }
+    }
+
+    std::cout << "merge of:" << std::endl << "\t\"" << leftstr << "\"" << std::endl << "with:" << std::endl << "\t" << "\"" << rightstr << "\"" <<
+                 std::endl << "results:" << std::endl << "\t\t\"" << merged << "\"" << std::endl;
+}
+
+int main() {
+
+    typedef std::vector< diffpp::edit > sln_t;
+    std::cout<< "strA=";std::cin>>leftstr;
+    std::cout<< "strB=";std::cin>>rightstr;
+
+    sln_t sln;
+    sln_t slnb;
+
+    auto pred = [](const char &a, const char &b) { return a == b; };
+    diffpp::algorithms::diff_greedyfwd(std::begin(leftstr), std::end(leftstr), std::begin(rightstr), std::end(rightstr), sln, pred);
+    diffpp::algorithms::diff_greedybwd(std::begin(leftstr), std::end(leftstr), std::begin(rightstr), std::end(rightstr), slnb, pred );
+
+    print_diff_merge(sln);
+    print_diff_merge(slnb);
+
+    return 0;
+}
+
